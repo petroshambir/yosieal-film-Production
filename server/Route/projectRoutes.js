de6cdbce9273874d2e2d9f53b64af198748f9072
unknown <petroshambirr@gmail.com>
2026-08-25 17:47:45 +0300

    fix project sinttax

--- server/Route/projectRoutes.js
+++ server/Route/projectRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 // import Project from '../models/project.js'; // ኣብ ኣይነት ናይ ፕሮጀክት ሞዴል ኣሎ።
-import Project from "../models/Project.js";
+import Project from "../models/project.js";
 //
 import { upload } from '../cloudinaryConfig.js'
 const router = express.Router();
